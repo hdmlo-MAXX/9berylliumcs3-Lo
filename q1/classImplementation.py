d266e0f28a1de31f11bb2b0794ef4636a45cc410
hdmlo-MAXX <hdmlo@brc.pshs.edu.ph>
2026-09-08 12:26:34 +0800

    Gogogo

--- q1/classImplementation.py
+++ q1/classImplementation.py
@@ -9,10 +9,12 @@ class FootballPlayer:
         print(f"Name: {self.Name}")
         print(f"Age: {self.Age}")
         print(f"Club: {self.Club}")
+        return self.Name, self.Age, self.Club   
 
      def ShowWages(self):
         wage = len(self.Name) * 1000
         print(f"{self.Name} earns {wage} per week.")
+        return wage
 
     def FollowPlayer(self):
         choose = print(f" Would you like to follow {self.Name}?")
@@ -20,4 +22,9 @@ class FootballPlayer:
             print(f"You are now following {self.Name}")
         elif choose == False:
             print(f"You are not following {self.Name}")
+        return choose
 
+FootballPlayer1 = FootballPlayer("Lionel Messi", 36, "Inter Miami")
+FootballPlayer2 = FootballPlayer("Cristiano Ronaldo", 38, "Al Nassr")
+
+FootballPlayer1.DisplayPlayerInfo()
