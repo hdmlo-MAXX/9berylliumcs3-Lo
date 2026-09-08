21cfffd57a41e14d831248f5e2dd943a3900382a
hdmlo-MAXX <hdmlo@brc.pshs.edu.ph>
2026-09-08 13:01:35 +0800

    Code

--- q1/classImplementation.py
+++ q1/classImplementation.py
@@ -1,9 +1,10 @@
 class FootballPlayer:
     def __init__(self, Name, Age, Club, WagePW):
-    self.Name = Name
-    self.Age = Age
-    self.Club = Club
-    self.__WagePW = WagePW
+      self.Name = Name
+      self.Age = Age
+      self.Club = Club
+      self.WagePW = WagePW
+      self.Follow = False
 
     def DisplayPlayerInfo(self):
         print(f"Name: {self.Name}")
@@ -11,20 +12,28 @@ class FootballPlayer:
         print(f"Club: {self.Club}")
         return self.Name, self.Age, self.Club   
 
-     def ShowWages(self):
-        wage = len(self.Name) * 1000
-        print(f"{self.Name} earns {wage} per week.")
-        return wage
+    def ShowWages(self):
+        WagePW = len(self.Name) * 1000
+        print(f"{self.Name} earns {WagePW} per week.")
+        return WagePW
 
     def FollowPlayer(self):
-        choose = print(f" Would you like to follow {self.Name}?")
-        if choose == True:
+        choose = input(f"Would you like to follow {self.Name}?: ")
+        if choose in ['yes', 'y', 'Yes', 'Y']:
             print(f"You are now following {self.Name}")
-        elif choose == False:
+            return True
+        elif choose in ['no', 'n', 'No', 'N']:
             print(f"You are not following {self.Name}")
+            return False
         return choose
 
-FootballPlayer1 = FootballPlayer("Lionel Messi", 36, "Inter Miami")
-FootballPlayer2 = FootballPlayer("Cristiano Ronaldo", 38, "Al Nassr")
+FootballPlayer1 = FootballPlayer("Lionel Messi", 36, "Inter Miami", 0)
+FootballPlayer2 = FootballPlayer("Cristiano Ronaldo", 38, "Al Nassr", 0)
 
 FootballPlayer1.DisplayPlayerInfo()
+FootballPlayer1.ShowWages()
+FootballPlayer1.FollowPlayer()
+
+FootballPlayer2.DisplayPlayerInfo()
+FootballPlayer2.ShowWages()
+FootballPlayer2.FollowPlayer()
